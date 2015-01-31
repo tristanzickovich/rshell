d1055cae84f18860bc727ebdab170116699de647
Tristan Zickovich <tzick002@ucr.edu>
2015-01-30 16:01:16 -0800

    basic opendir() addition

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <dirent.h>
+#include <sys/types.h>
+#include <errno.h>
 
 using namespace std;
 
 int main(){
+	const char *path = ".";
+	DIR * pdir = opendir(path); //return a pointer to dir
+	dirent *direntp;
+	while(direntp = readdir(pdir))
+		cout << direntp->d_name << endl;
 
+	closedir(pdir);
 	return 0;
 }
