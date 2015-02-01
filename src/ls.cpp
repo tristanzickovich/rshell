8950b310a07038ec37e6ce56d5ca80c5e79678b4
Tristan Zickovich <tzick002@ucr.edu>
2015-01-31 22:14:45 -0800

    began work on mult files for -l

--- src/ls.cpp
+++ src/ls.cpp
@@ -29,73 +29,83 @@ void printa(const char *path){
 
 void printl(){
 	struct stat sb;
-
-	if(stat("a.out", &sb) == -1){
-		perror("Stat Error:");
+	DIR * pdir = opendir("."); //return a pointer to dir
+	if(pdir == NULL){
+		perror("Can't open directory");
 		exit(1);
 	}
-	//set owner of item to ownr
-	struct passwd *pass = getpwuid(sb.st_uid);
-	char *ownr = pass->pw_name;	
-	//set group owner to gownr
-	struct group *grp = getgrgid(sb.st_gid);
-	char *gownr = grp->gr_name; 
-	//output permissions line
-	if(S_ISDIR(sb.st_mode))
-		cout << 'd';
-	else if(S_ISLNK(sb.st_mode))
-		cout << 's';
-	else
-		cout << '-';
-	if(S_IRUSR & sb.st_mode)
-		cout << 'r';
-	else
-		cout << '-';
-	if(S_IWUSR & sb.st_mode)
-		cout << 'w';
-	else
-		cout << '-';
-	if(S_IXUSR & sb.st_mode)
-		cout << 'x';
-	else
-		cout << '-';
-	if(S_IRGRP & sb.st_mode)
-		cout << 'r';
-	else
-		cout << '-';
-	if(S_IWGRP & sb.st_mode)
-		cout << 'w';
-	else
-		cout << '-';
-	if(S_IXGRP & sb.st_mode)
-		cout << 'x';
-	else
-		cout << '-';
-	if(S_IROTH & sb.st_mode)
-		cout << 'r';
-	else
-		cout << '-';
-	if(S_IWOTH & sb.st_mode)
-		cout << 'w';
-	else
-		cout << '-';
-	if(S_IXOTH & sb.st_mode)
-		cout << 'x';
-	else
-		cout << '-';
-	//number of links
-	cout << ' ' << sb.st_nlink;	
-	//owner of item
-	cout << ' ' << ownr;
-	//group owner of item
-	cout << ' ' << gownr;
-	//size of item in bytes
-	cout << ' ' << sb.st_size;
-	//time item was last modified
-	string lastmodtime = ctime(&sb.st_mtime);
-	cout << ' ' << lastmodtime.substr(4,12);
-	//add last output of file name FIXME
-	cout << endl;
+	dirent *direntp;
+	while(direntp = readdir(pdir)){
+		char *path = direntp->d_name;
+
+		if(stat(path, &sb) == -1){
+			perror("Stat Error:");
+			exit(1);
+		}
+		//set owner of item to ownr
+		struct passwd *pass = getpwuid(sb.st_uid);
+		char *ownr = pass->pw_name;	
+		//set group owner to gownr
+		struct group *grp = getgrgid(sb.st_gid);
+		char *gownr = grp->gr_name; 
+		//output permissions line
+		if(S_ISDIR(sb.st_mode))
+			cout << 'd';
+		else if(S_ISLNK(sb.st_mode))
+			cout << 's';
+		else
+			cout << '-';
+		if(S_IRUSR & sb.st_mode)
+			cout << 'r';
+		else
+			cout << '-';
+		if(S_IWUSR & sb.st_mode)
+			cout << 'w';
+		else
+			cout << '-';
+		if(S_IXUSR & sb.st_mode)
+			cout << 'x';
+		else
+			cout << '-';
+		if(S_IRGRP & sb.st_mode)
+			cout << 'r';
+		else
+			cout << '-';
+		if(S_IWGRP & sb.st_mode)
+			cout << 'w';
+		else
+			cout << '-';
+		if(S_IXGRP & sb.st_mode)
+			cout << 'x';
+		else
+			cout << '-';
+		if(S_IROTH & sb.st_mode)
+			cout << 'r';
+		else
+			cout << '-';
+		if(S_IWOTH & sb.st_mode)
+			cout << 'w';
+		else
+			cout << '-';
+		if(S_IXOTH & sb.st_mode)
+			cout << 'x';
+		else
+			cout << '-';
+		//number of links
+		cout << ' ' << sb.st_nlink;	
+		//owner of item
+		cout << ' ' << ownr;
+		//group owner of item
+		cout << ' ' << gownr;
+		//size of item in bytes
+		cout << ' ' << sb.st_size;
+		//time item was last modified
+		string lastmodtime = ctime(&sb.st_mtime);
+		cout << ' ' << lastmodtime.substr(4,12);
+		//add last output of file name FIXME
+		cout << endl;
+	}
+	closedir(pdir);
 }
 
 void printr(){
@@ -136,6 +146,20 @@ int main(int argc, char *argv[]){
 		else if (string(argv[i]) == rflag)
 			printr();
 	}*/
-	cout << countflags(argc, argv) << endl;
+	int numflags = countflags(argc, argv);
+	
+	//if flag is -a
+	if(numflags == 1)
+		printa(".");
+	//if flag is -l
+	else if(numflags == 2)
+		printl();
 	return 0;
 }
+
+
+
+
+
+
+
